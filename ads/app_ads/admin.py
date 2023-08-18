af66f73d1e9980cbd6e1b80c44cfa5189dc2353f
Pasha <alna-hozyasheva@gmail.com>
2023-08-18 17:43:29 +0500

    Задания к уроку 8

--- ads/app_ads/admin.py
+++ ads/app_ads/admin.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
+from .models import Advertisement
 
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'created_date','updated_date', 'auction'] # выводим данные таблицы в админке
+    list_filter = ['auction', 'created_at'] # Задаем фильтрацию
+    actions = ['make_action_at_false', 'make_action_at_true'] # Зададим действия с объектами таблицы и создадим данные фукнции, обернув в декоратор
 
-# Register your models here.
+    fieldsets = ( # При создании объявления, разделим данные на категории
+        ('Общее', {
+            'fields' : ('title','description')
+            }),
+
+        ('Финансы', {
+            'fields' : ('price','auction'),
+            'classes' : ['collapse'], # для создания выпадающего меню
+            }),
+    )
+
+    @admin.action(description='Убрать возможность торга')
+    def make_action_at_false(self, request, queryset):
+        queryset.update(auction=False) # Если включили эту функцию, то будет ложь
+        
+    @admin.action(description='Добавить возможность торга')
+    def make_action_at_true(self, request, queryset):
+        queryset.update(auction=True) # Если включили эту функцию, то будет правда
+
+admin.site.register(Advertisement, AdvertisementAdmin) # Позволяет зарегистрировать модель таблицы
 
     
--- ads/app_ads/models.py
+++ ads/app_ads/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib import admin
+from django.utils.html import format_html
 
 # Здесь создаются модели новых таблиц, затем нужно будет мигрировоть с помощью "python manage.py migrate" их в БД, но сначала их создать с помощью "python manage.py makemigrations" в миграциях
 class Advertisement(models.Model):
@@ -15,3 +16,22 @@ class Advertisement(models.Model):
     created_at = models.DateTimeField(auto_now_add=True) # Поле для даты создания, auto_now_add - при создании объявления
     updated_at = models.DateTimeField(auto_now=True) # Поле для даты обновления, auto_now - при обновлении
     
+    @admin.display(description='дата создания')
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date(): # если дата создания-сегодня
+            created_time = self.created_at.time().strftime("%H:%M:%S") # добавим время
+            return format_html(
+                '<span style="color: red; font-weight: bold;">Сегодня в {}</span>', created_time # вернем в нужном формате + время
+            )
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S") # иначе другой формат
+    
+    @admin.display(description='дата обновления')
+    def updated_date(self):
+        from django.utils import timezone
+        if self.updated_at.date() == timezone.now().date(): # если дата создания-сегодня
+            updated_time = self.updated_at.time().strftime("%H:%M:%S") # добавим время
+            return format_html(
+                '<span style="color: blue; font-weight: bold;">Сегодня в {}</span>', updated_time # вернем в нужном формате + время
+            )
+        return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S") # иначе другой формат
